c5c10f68cad88aa96e082e24cce2b6a51234648e
Mace Ojala <mace.ojala@gmail.com>
2025-08-09 01:23:50 +0100

    Change outputs 3/4 to derivates. More parameters.

--- README.md
+++ README.md
@@ -9,13 +9,13 @@ Accumulate shiny things! Dual integrator for crow
     → 1 signal 1
     → 2 signal 2
       1 signal 1 integral  →
-      2 signal 1 rising    →
+      2 signal 1 derivate  →
       3 signal 2 integral  →
-      4 signal 2 rising    →
+      4 signal 2 derivate  →
 
 Accumulate all the shiny things! A pair of integrators, meaning two accumulators are collecting from inputs 1 and 2, capped by hardware limits at -5 an 10 volts.
 
-Non-linear response is hopefully useful/interesting at slow rates near 0 as well as higher voltages. There are two exponential factors `bend1` and `bend2` you can tune in the public variables to retune the response via druid, bowering or livecoding from maiden. Low values make more room around 0V, high values overrespond towards squares for higher rates.
+Non-linear response is hopefully useful/interesting at slow rates near 0 as well as higher voltages. There are exponential `i_bend` and linear `d_div` factors you can tune to change mapping voltage. They are crow public variables, change them via druid, bowering or livecoding from maiden. Low integral bend values make more room around 0V, high values overrespond towards squares for higher rates. Lower derivate divisor values yield mellower values.
 
 No shortage of glitchy aliasing effect etc.
 
--- shinythings.lua
+++ shinythings.lua
@@ -3,9 +3,9 @@
 -- → 1 signal 1
 -- → 2 signal 2
 --   1 signal 1 integral  →
---   2 signal 1 rising    →
+--   2 signal 1 derivate  →
 --   3 signal 2 integral  →
---   4 signal 2 rising    →
+--   4 signal 2 derivate  →
 --
 -- by xmacex
 
@@ -15,14 +15,14 @@ local MAXV = 10
 local TRUEV  = 5
 local FALSEV = 0
 
-local FREQ   = 333
+local FREQ   = 444
 
 local integrals = {0, 0}
-local rising    = {false, false}
+local derivates = {0, 0}
 local dt        = 1/FREQ
 
-public{bend1 = 7}:range(0, 12)
-public{bend2 = 7}:range(0, 12)
+public{i_bend = {7, 7}}:range(0, 12)
+public{d_div  = {10, 10}}:range(1, 100)
 
 function init()
    output[1].slew = 1/FREQ
@@ -35,33 +35,34 @@ function init()
 end
 
 function process1(v)
-   -- dv = integrate(v, dt)*public.timemult
-   local dv = integrate(v, dt)*math.exp(public.bend1)
+   local dv = integrate(v, dt)*math.exp(public.i_bend[1])
    integrals[1] = clamp(integrals[1]+dv, MINV, MAXV)
-   rising[1] = dv > 0
+   derivates[1] = derivate(integrals[1], output[1].volts, dt)
 
    output[1].volts = integrals[1]
-   if rising[1] then output[2].volts = TRUEV else output[2].volts = FALSEV end
+   output[2].volts = derivates[1] / public.d_div[1]
 end
 
 function process2(v)
-   -- dv = integrate(v, dt)*public.timemult
-   local dv = integrate(v, dt)*math.exp(public.bend2)
+   local dv = integrate(v, dt)*math.exp(public.i_bend[2])
    integrals[2] = clamp(integrals[2]+dv, MINV, MAXV)
-   rising[2] = dv > 0
+   derivates[2] = derivate(integrals[2], output[3].volts, dt)
 
    output[3].volts = integrals[2]
-   if rising[2] then output[4].volts = TRUEV else output[4].volts = FALSEV end
+   output[4].volts = derivates[2] / public.d_div[2]
 end
 
 function integrate(v, dt)
    return v/(1/dt)
 end
 
+function derivate(v, v_, dt)
+   return (v - v_)/dt
+end
+
 function clamp(v, min, max)
    return math.max(min, math.min(v, max))
 end
-
 
 -- Local Variables:
 -- flycheck-luacheck-standards: ("crow")
